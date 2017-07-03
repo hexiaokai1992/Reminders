28933a8076d4316def0be7abffc3d048fd3d2289
hexiaokai1992 <hexiaokai1992@126.com>
2017-07-03 19:10:56 +0800

    开启后自动清楚列表并添加备忘 可以点击条目了

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,17 +1,59 @@
 package com.apress.gerber.reminders;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class RemindersActivity extends ActionBarActivity {
+//    private ListView mListView;
+//    private RemindersDbAdapter mDbAdapter;
+//    private RemindersSimpleCursorAdapter mCursorAdapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_reminders);
+//        mListView = (ListView) findViewById(R.id.reminders_list_view);
+//        mListView.setDivider(null);
+//        mDbAdapter = new RemindersDbAdapter(this);
+//        mDbAdapter.open();
+//        Cursor cursor = mDbAdapter.fetchAllReminders();
+////from columns defined in the db
+//        String[] from = new String[]{
+//                RemindersDbAdapter.COL_CONTENT
+//        };
+////to the ids of views in the layout
+//        int[] to = new int[]{
+//                R.id.row_text
+//        };
+//        mCursorAdapter = new RemindersSimpleCursorAdapter(
+////context
+//                RemindersActivity.this,
+////the layout of the row
+//                R.layout.reminders_row,
+////cursor
+//                cursor,
+////from columns defined in the db
+//                from,
+////to the ids of views in the layout
+//                to,
+////flag - not used
+//                0);
+//// the cursorAdapter (controller) is now updating the listView (view)
+////with data from the db (model)
+//        mListView.setAdapter(mCursorAdapter);
+//    }
+//Abbreviated for brevity
+
 
     private ListView mListView;
     private RemindersDbAdapter mDbAdapter;
@@ -24,7 +66,13 @@ public class RemindersActivity extends ActionBarActivity {
         mListView.setDivider(null);
         mDbAdapter = new RemindersDbAdapter(this);
         mDbAdapter.open();
-        Cursor cursor = mDbAdapter.fetchAllReminders();
+        if (savedInstanceState == null) {
+//Clear all data
+            mDbAdapter.deleteAllReminders();
+//Add some data
+            insertSomeReminders();
+            Log.v("已添加","所有备忘");
+                    Cursor cursor = mDbAdapter.fetchAllReminders();
 //from columns defined in the db
         String[] from = new String[]{
                 RemindersDbAdapter.COL_CONTENT
@@ -49,7 +97,102 @@ public class RemindersActivity extends ActionBarActivity {
 // the cursorAdapter (controller) is now updating the listView (view)
 //with data from the db (model)
         mListView.setAdapter(mCursorAdapter);
+        }
+//Removed remaining method code for brevity...
+
+        //when we click an individual item in the listview
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(RemindersActivity.this, "clicked " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
-//Abbreviated for brevity
+
+    private void insertSomeReminders() {
+        mDbAdapter.createReminder("Buy Learn Android Studio", true);
+        mDbAdapter.createReminder("Send Dad birthday gift", false);
+        mDbAdapter.createReminder("Dinner at the Gage on Friday", false);
+        mDbAdapter.createReminder("String squash racket", false);
+        mDbAdapter.createReminder("Shovel and salt walkways", false);
+        mDbAdapter.createReminder("Prepare Advanced Android syllabus", true);
+        mDbAdapter.createReminder("Buy new office chair", false);
+        mDbAdapter.createReminder("Call Auto-body shop for quote", false);
+        mDbAdapter.createReminder("Renew membership to club", false);
+        mDbAdapter.createReminder("Buy new Galaxy Android phone", true);
+        mDbAdapter.createReminder("Sell old Android phone - auction", false);
+        mDbAdapter.createReminder("Buy new paddles for kayaks", false);
+        mDbAdapter.createReminder("Call accountant about tax returns", false);
+        mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
+        mDbAdapter.createReminder("Call the Dalai Lama back", true);
+    }
+//Removed remaining method code for brevity...brevity
+
+
+    //------------------------加载溢出菜单 不能动
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+//create new Reminder
+                Log.d(getLocalClassName(),"create new Reminder");
+                return true;
+//            case R.id.clean_all:
+//                Log.v("清除","清除所有备忘录");
+//                mDbAdapter.deleteAllReminders();
+//                refresh();
+//                return true;
+//            case  R.id.add_all:
+//                Log.v("添加","添加所有备忘录");
+//                insertSomeReminders();
+//                refresh();
+//                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    //刷新当前Activity
+    private void refresh() {
+        finish();
+        Intent intent = new Intent(RemindersActivity.this, RemindersActivity.class);
+        startActivity(intent);
+    }
+//        private void insertSomeReminders() {
+//        mDbAdapter.createReminder("Buy Learn Android Studio", true);
+//        mDbAdapter.createReminder("Send Dad birthday gift", false);
+//        mDbAdapter.createReminder("Dinner at the Gage on Friday", false);
+//        mDbAdapter.createReminder("String squash racket", false);
+//        mDbAdapter.createReminder("Shovel and salt walkways", false);
+//        mDbAdapter.createReminder("Prepare Advanced Android syllabus", true);
+//        mDbAdapter.createReminder("Buy new office chair", false);
+//        mDbAdapter.createReminder("Call Auto-body shop for quote", false);
+//        mDbAdapter.createReminder("Renew membership to club", false);
+//        mDbAdapter.createReminder("Buy new Galaxy Android phone", true);
+//        mDbAdapter.createReminder("Sell old Android phone - auction", false);
+//        mDbAdapter.createReminder("Buy new paddles for kayaks", false);
+//        mDbAdapter.createReminder("Call accountant about tax returns", false);
+//        mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
+//        mDbAdapter.createReminder("Call the Dalai Lama back", true);
+//    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        MenuInflater inflator = new MenuInflater(this);
+        // 装载R.menu.my_menu对应的菜单，并添加到menu中
+        inflator.inflate(R.menu.menu_reminders, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 }
+
+//public class RemindersActivity extends ActionBarActivity {
+
+//}
+
+
