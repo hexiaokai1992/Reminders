944e0c9f8b91d7ba9f64efcaec6a6e6035789781
hexiaokai1992 <hexiaokai1992@126.com>
2017-07-03 12:06:38 +0800

    完成了

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,5 +1,7 @@
 package com.apress.gerber.reminders;
 
+import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,44 +11,45 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class RemindersActivity extends AppCompatActivity {
+public class RemindersActivity extends ActionBarActivity {
 
     private ListView mListView;
-
-
-
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursorAdapter mCursorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
         mListView = (ListView) findViewById(R.id.reminders_list_view);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
+        mListView.setDivider(null);
+        mDbAdapter = new RemindersDbAdapter(this);
+        mDbAdapter.open();
+        Cursor cursor = mDbAdapter.fetchAllReminders();
+//from columns defined in the db
+        String[] from = new String[]{
+                RemindersDbAdapter.COL_CONTENT
+        };
+//to the ids of views in the layout
+        int[] to = new int[]{
+                R.id.row_text
+        };
+        mCursorAdapter = new RemindersSimpleCursorAdapter(
+//context
+                RemindersActivity.this,
+//the layout of the row
                 R.layout.reminders_row,
-                R.id.row_text, new String[]{"first record", "second record", "third record"}
-        );
-        mListView.setAdapter(arrayAdapter);
+//cursor
+                cursor,
+//from columns defined in the db
+                from,
+//to the ids of views in the layout
+                to,
+//flag - not used
+                0);
+// the cursorAdapter (controller) is now updating the listView (view)
+//with data from the db (model)
+        mListView.setAdapter(mCursorAdapter);
     }
+//Abbreviated for brevity
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = new MenuInflater(this);
-        inflater.inflate(R.menu.menu_reminders,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-
-            case R.id.action_new:
-                Log.d(getLocalClassName(),"create new Reminder");
-                return true;
-            case R.id.action_exit:
-                finish();
-                return true;
-            default:
-                return false;
-        }
-    }
 }
